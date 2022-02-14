460fb8462cea1bb304f339b2e7cd38d63a535466
knownrock <knownrock@hotmail.com>
2022-02-14 22:11:03 +0800

    optimize display

--- traverseAstToString.ts
+++ traverseAstToString.ts
@@ -3,12 +3,12 @@ function traverseAstToString(ast, tab = ''){
   let str = ``
   if(ast.type === 'NODE'){
     // console.log(tab + ast.id)
-    str += '' + tab + '+' + ast.id
+    str += '' + tab + 'n:[' + ast.id+']'
     for(let i = 0; i < ast.node.length; i++){
-      str += '\n' + traverseAstToString(ast.node[i], tab + '\t')
+      str += '\n' + traverseAstToString(ast.node[i], tab + '  ')
     }
   }else if(ast.type === 'WORD'){
-    str += '' + (tab +  '|' + ast.id + ':' + ast.token.value)
+    str += '' + (tab  + 'w:[' + ast.id + ':' + ast.token.value + ']')
   }
   return str
 }
